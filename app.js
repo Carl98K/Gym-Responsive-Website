c3315111ee9fdccaaa37b3625dcfbd32a322f759
Etzio <carldrewx@gmail.com>
2025-05-02 22:51:18 +0800

    Finish site

---
+++ app.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const sidebar = document.getElementById("sidebar-list");
+    sidebar.style.display = "flex";
+}
+
+function hideSideBar() {
+    const sidebar = document.getElementById("sidebar-list");
+    sidebar.style.display = "none";
+}
